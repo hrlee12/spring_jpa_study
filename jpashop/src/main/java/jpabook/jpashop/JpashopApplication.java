f03f807a5ce485f625fcdc64a8b00197fa074683
hyori <gyfl5542@naver.com>
2023-12-16 04:05:18 +0900

    initialize

---
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopApplication.class, args);
+		LombokTest test = new LombokTest();
+		System.out.println(test.getName());
+		test.setName("myName");
+		System.out.println(test.getName());
+
+	}
+
+}
---
+++ jpashop/src/main/java/jpabook/jpashop/LombokTest.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LombokTest {
+    public String name;
+}
